918b73d081be077b12ab4d7f4c36e6df24a258c5
mauriramos10 <mauriramos10@gmail.com>
2026-02-01 21:10:45 -0600

    config.js

---
+++ docs/config.js
@@ -0,0 +1,13 @@
+window.FERTI_FIREBASE = {
+  firebaseConfig: {
+    apiKey: "PEGA_AQUI",
+    authDomain: "PEGA_AQUI",
+    projectId: "PEGA_AQUI",
+    storageBucket: "PEGA_AQUI",
+    messagingSenderId: "PEGA_AQUI",
+    appId: "PEGA_AQUI"
+  },
+
+  // todos los dispositivos usan este mismo nombre
+  workspace: "ferti-mm"
+};
